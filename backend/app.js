6cc92be004b50b345085dd373f47d7ab3083ee0e
BulandaK <eskimosl@wp.pl>
2025-05-26 16:44:49 +0200

    fix(app): remove unnecessary route

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors"); // Import pakietu cors
+const cors = require("cors");
 const dbSetup = require("./db");
 const routes = require("./routes");
-const favoriteRoutes = require("./routes/userFavoriteRoutes");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpecs = require("./swagger");
@@ -17,7 +16,6 @@ notificationService.connectRabbitMQ().catch((err) => {
   console.error("Initial RabbitMQ connection failed:", err);
 });
 
-// Konfiguracja CORS
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -31,7 +29,6 @@ app.use(passport.initialize());
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use("/api", routes);
-app.use("/api/favorites", favoriteRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello, world!" });
